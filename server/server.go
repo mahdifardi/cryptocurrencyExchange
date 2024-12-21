7b2c1d7ac5ed1350ac13b07a5b170c65e92b8ac2
mahdifardi <mrmahdifardi@gmail.com>
2024-12-21 20:53:39 +0000

    market-maker-alg bugfix

--- server/server.go
+++ server/server.go
@@ -221,6 +221,11 @@ type PriceResponse struct {
 	Price float64
 }
 
+type GetOrdersResponse struct {
+	Asks []Order
+	Bids []Order
+}
+
 func (ex *Exchange) handleGetOrders(c echo.Context) error {
 	userIdStr := c.Param("userId")
 	userId, err := strconv.Atoi(userIdStr)
@@ -228,13 +233,28 @@ func (ex *Exchange) handleGetOrders(c echo.Context) error {
 		return err
 	}
 
-	ex.mu.Lock()
-	defer ex.mu.Unlock()
+	ex.mu.RLock()
+	defer ex.mu.RUnlock()
 	orderBookOrders := ex.Orders[int64(userId)]
 
-	orders := make([]Order, len(orderBookOrders))
+	orderResponse := &GetOrdersResponse{
+		Asks: []Order{},
+		Bids: []Order{},
+	}
+
+	// orders := make([]Order, len(orderBookOrders))
 
 	for i := 0; i < len(orderBookOrders); i++ {
+		// it could be that ther order is geetting filled even its included in this response we must double check if the limits is not nil
+
+		if orderBookOrders[i].Limit == nil {
+			fmt.Println("#################################")
+			fmt.Printf("the limmit of the order is nil %+v\n", orderBookOrders[i])
+			fmt.Println("#################################")
+
+			continue
+
+		}
 		o := Order{
 			UserID:    orderBookOrders[i].UserId,
 			ID:        orderBookOrders[i].ID,
@@ -243,11 +263,17 @@ func (ex *Exchange) handleGetOrders(c echo.Context) error {
 			Bid:       orderBookOrders[i].Bid,
 			Timestamp: orderBookOrders[i].Timestamp,
 		}
-		orders[i] = o
+		// orders[i] = o
+
+		if o.Bid {
+			orderResponse.Bids = append(orderResponse.Bids, o)
+		} else {
+			orderResponse.Asks = append(orderResponse.Asks, o)
+		}
 	}
 
 	// fmt.Printf("%+v", orders)
-	return c.JSON(http.StatusOK, orders)
+	return c.JSON(http.StatusOK, orderResponse)
 }
 
 func (ex *Exchange) handleGetBestBid(c echo.Context) error {
