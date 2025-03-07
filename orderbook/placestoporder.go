8c5326e32db15d8ff1abf4b99e847e3a90713162
mahdifardi <mrmahdifardi@gamil.com>
2025-03-07 16:32:48 +0000

    concurrent read, write bugfix

--- orderbook/placestoporder.go
+++ orderbook/placestoporder.go
@@ -1,7 +1,6 @@
 package orderbook
 
 import (
-	"fmt"
 	"math/big"
 
 	"github.com/mahdifardi/cryptocurrencyExchange/order"
@@ -12,26 +11,34 @@ func (ob *Orderbook) PlaceStopOrder(o *order.StopOrder, market order.Market, use
 
 	if o.Bid {
 		quoteAmount := new(big.Int).Mul(big.NewInt(int64(o.Price)), big.NewInt(int64(o.Size)))
-		userQuoteAvailableBalance := user.AssetBalances[order.Asset(market.Quote)].AvailableBalance
-		if userQuoteAvailableBalance.Cmp(quoteAmount) < 0 {
-			return fmt.Errorf("insufficient user %s balance: have %s, need %s", market.Quote, userQuoteAvailableBalance, quoteAmount)
-		} else {
-			userQuoteBalance := user.AssetBalances[order.Asset(market.Quote)]
-			userQuoteBalance.AvailableBalance = new(big.Int).Sub(userQuoteBalance.AvailableBalance, quoteAmount)
-			userQuoteBalance.ReservedBalance = new(big.Int).Add(userQuoteBalance.ReservedBalance, quoteAmount)
-			user.AssetBalances[order.Asset(market.Quote)] = userQuoteBalance
+		err := user.ReserveBalance(market, quoteAmount, o.Bid)
+		if err != nil {
+			return err
 		}
+		// userQuoteAvailableBalance := user.AssetBalances[order.Asset(market.Quote)].AvailableBalance
+		// if userQuoteAvailableBalance.Cmp(quoteAmount) < 0 {
+		// 	return fmt.Errorf("insufficient user %s balance: have %s, need %s", market.Quote, userQuoteAvailableBalance, quoteAmount)
+		// } else {
+		// 	userQuoteBalance := user.AssetBalances[order.Asset(market.Quote)]
+		// 	userQuoteBalance.AvailableBalance = new(big.Int).Sub(userQuoteBalance.AvailableBalance, quoteAmount)
+		// 	userQuoteBalance.ReservedBalance = new(big.Int).Add(userQuoteBalance.ReservedBalance, quoteAmount)
+		// 	user.AssetBalances[order.Asset(market.Quote)] = userQuoteBalance
+		// }
 	} else {
 		BaseAmount := big.NewInt(int64(o.Size))
-		userBaseBalance := user.AssetBalances[order.Asset(market.Base)].AvailableBalance
-		if userBaseBalance.Cmp(BaseAmount) < 0 {
-			return fmt.Errorf("insufficient user %s balance: have %s, need %s", market.Base, userBaseBalance, BaseAmount)
-		} else {
-			userBaseBalance := user.AssetBalances[order.Asset(market.Base)]
-			userBaseBalance.AvailableBalance = new(big.Int).Sub(userBaseBalance.AvailableBalance, BaseAmount)
-			userBaseBalance.ReservedBalance = new(big.Int).Add(userBaseBalance.ReservedBalance, BaseAmount)
-			user.AssetBalances[order.Asset(market.Base)] = userBaseBalance
+		err := user.ReserveBalance(market, BaseAmount, o.Bid)
+		if err != nil {
+			return err
 		}
+		// userBaseBalance := user.AssetBalances[order.Asset(market.Base)].AvailableBalance
+		// if userBaseBalance.Cmp(BaseAmount) < 0 {
+		// 	return fmt.Errorf("insufficient user %s balance: have %s, need %s", market.Base, userBaseBalance, BaseAmount)
+		// } else {
+		// 	userBaseBalance := user.AssetBalances[order.Asset(market.Base)]
+		// 	userBaseBalance.AvailableBalance = new(big.Int).Sub(userBaseBalance.AvailableBalance, BaseAmount)
+		// 	userBaseBalance.ReservedBalance = new(big.Int).Add(userBaseBalance.ReservedBalance, BaseAmount)
+		// 	user.AssetBalances[order.Asset(market.Base)] = userBaseBalance
+		// }
 	}
 
 	if o.Limit {
