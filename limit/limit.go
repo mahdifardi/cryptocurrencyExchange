46b1f5e5183226c0bf0ad21a76cf0d04b85b3105
mahdifardi <mrmahdifardi@gamil.com>
2025-05-09 13:49:45 +0000

    refactor Sort

--- limit/limit.go
+++ limit/limit.go
@@ -12,16 +12,19 @@ type Limit struct {
 
 type Limits []*Limit
 
+func (l Limits) Len() int      { return len(l) }
+func (l Limits) Swap(i, j int) { l[i], l[j] = l[j], l[i] }
+
 type ByBestAsk struct{ Limits }
 
-func (a ByBestAsk) Len() int           { return len(a.Limits) }
-func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
+// func (a ByBestAsk) Len() int           { return len(a.Limits) }
+// func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
 func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
 
 type ByBestBid struct{ Limits }
 
-func (b ByBestBid) Len() int           { return len(b.Limits) }
-func (b ByBestBid) Swap(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
+// func (b ByBestBid) Len() int           { return len(b.Limits) }
+// func (b ByBestBid) Swap(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
 func (b ByBestBid) Less(i, j int) bool { return b.Limits[i].Price > b.Limits[j].Price }
 
 func NewLimit(price float64) *Limit {
